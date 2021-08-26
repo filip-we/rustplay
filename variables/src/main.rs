702e5601fc231ebf005a0942546d70c4066378d5
filip-we <filip.wettlen@gmail.com>
2021-08-26 22:49:09 +0200

    WIP

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,34 @@
+use std::io;
+
 const MAX_POINTS: u32 = 100_000;
 
 fn main() {
     let x = 5;
+    println!("The value of x is: {}", x);
     let x = 10;
     println!("The value of x is: {}", x);
+
+    let a = [1, 2, 3, 4, 5];
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("message");
+
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("needs to be number");
+
+
+    let element = a[index];
+
+    println!(
+        "element at {} is {}",
+        index,
+        element
+       );
+
 }
